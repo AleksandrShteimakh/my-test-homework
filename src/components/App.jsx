083afde79ca8ffb99ccd339168c1d36c1e2ba259
AleksandrShteimakh <107569995+AleksandrShteimakh@users.noreply.github.com>
2023-01-10 13:51:06 +0100

    Fine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Statistics } from './Statistics/Statistics.jsx'
+// import data from './FilesJSON/data.json'
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return (<div>
+<Statistics />
+  </div> 
+  )
 };
+
